0935a42768d0e79af77e72caf518c589521ebcab
lnanh2k4 <lnanh2k4@gmail.com>
2024-10-11 19:36:51 +0700

    add login form, dashboard, homepage Windows and Pages

--- Views/Pages/Dashboard/VoucherManagement.xaml.cs
+++ Views/Pages/Dashboard/VoucherManagement.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -9,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Capybook
+namespace Capybook.Views.Pages.Dashboard
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for VoucherManagement.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class VoucherManagement : Page
     {
-        public MainWindow()
+        public VoucherManagement()
         {
             InitializeComponent();
         }
---
+++ Views/Windows/Dashboard.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Capybook.Views.Windows
+{
+    /// <summary>
+    /// Interaction logic for Dashboard.xaml
+    /// </summary>
+    public partial class Dashboard : Window
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogout_Click(object sender, RoutedEventArgs e)
+        {
+            Window login = new Login();
+            this.Close();
+            login.Show();
+        }
+    }
+}
