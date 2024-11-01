95e20eccb6ad08205d11eab68138743b456aff02
Khanh <khanhdcce180117@fpt.edu.vn>
2024-11-01 09:48:04 +0700

    commit change

--- obj/Debug/net8.0-windows/Views/Windows/Homepage.g.i.cs
+++ obj/Debug/net8.0-windows/Views/Windows/Homepage.g.i.cs
@@ -93,7 +93,7 @@ namespace Capybook.Views.Windows {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Capybook;V1.0.0.0;component/views/windows/homepage.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Capybook;component/views/windows/homepage.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\Views\Windows\Homepage.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
