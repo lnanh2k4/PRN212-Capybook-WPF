8ea44bf2cf2d32c08da192084294b9d272e90107
lnanh2k4 <lnanh2k4@gmail.com>
2024-11-02 19:48:09 +0700

    Modify UI Account Management

--- Views/Windows/Register.xaml.cs
+++ Views/Windows/Register.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Capybook.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,8 @@ namespace Capybook.Views.Windows
         public Register()
         {
             InitializeComponent();
+            DataContext = new AccountVM();
+            
         }
     }
 }
